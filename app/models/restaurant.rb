3a003279a871292d5bdb60730d0a21e9cc5a8da0
adrien taravant <adrien.taravant@gmail.com>
2017-01-31 15:21:05 +0100

    yelp terminé

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,10 @@
 class Restaurant < ApplicationRecord
+
+ CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
  has_many :reviews, inverse_of: :restaurant, dependent: :destroy
  validates :name, :address, presence: true
- validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+ validates :category, inclusion: { in: CATEGORIES }
+
 
 end
